a2a5bfed36331750fd14d09c204a44804c598cfd
reidn3r <rdn.adn00@gmail.com>
2025-09-06 13:06:22 -0300

    refactor: add. novos metodos e corrige identacao

--- url-redirect/src/main/java/com/providers/StorageProvider.java
+++ url-redirect/src/main/java/com/providers/StorageProvider.java
@@ -12,35 +12,35 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class StorageProvider {
 
-    private String containerName = "url-temp-file";
-    private BlobServiceClient client;
-    private ObjectMapper serializer;
+  private String containerName = "url-temp-file";
+  private BlobServiceClient client;
+  private ObjectMapper serializer;
 
-    public StorageProvider(ObjectMapper objectMapper) {
-        String endpoint = EnvironmentConfig.get("STORAGE_ENDPOINT");
-        String connString = EnvironmentConfig.get("CONN_STRING");
+  public StorageProvider(ObjectMapper objectMapper) {
+    String endpoint = EnvironmentConfig.get("STORAGE_ENDPOINT");
+    String connString = EnvironmentConfig.get("CONN_STRING");
 
-        this.serializer = objectMapper.registerModule(new JavaTimeModule())
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+    this.serializer = objectMapper.registerModule(new JavaTimeModule())
+      .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
-        this.client = new BlobServiceClientBuilder()
-                .endpoint(endpoint)
-                .connectionString(connString)
-                .buildClient();
+    this.client = new BlobServiceClientBuilder()
+      .endpoint(endpoint)
+      .connectionString(connString)
+      .buildClient();
+  }
+
+  public JsonFileDTO findBlobByName(String name) throws Exception {
+    String blobName = name + ".json";
+
+    BlobContainerClient containerClient = this.client.getBlobContainerClient(this.containerName);
+    BlobClient blobClient = containerClient.getBlobClient(blobName);
+
+    if (!blobClient.exists()) {
+      throw new Exception("URL not found");
     }
 
-    public JsonFileDTO findBlobByName(String name) throws Exception {
-        String blobName = name + ".json";
-
-        BlobContainerClient containerClient = this.client.getBlobContainerClient(this.containerName);
-        BlobClient blobClient = containerClient.getBlobClient(blobName);
-
-        if (!blobClient.exists()) {
-            throw new Exception("URL not found");
-        }
-
-        String fileStream = blobClient.downloadContent().toString();
-        return this.serializer.readValue(fileStream, JsonFileDTO.class);
-    }
+    String fileStream = blobClient.downloadContent().toString();
+    return this.serializer.readValue(fileStream, JsonFileDTO.class);
+  }
 
 }
--- url-redirect/src/main/java/com/redirect/Function.java
+++ url-redirect/src/main/java/com/redirect/Function.java
@@ -17,45 +17,62 @@ import com.providers.StorageProvider;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-/**
- * Azure Functions with HTTP Trigger.
- */
 public class Function {
-        private final ObjectMapper objectMapper = new ObjectMapper();
-        public StorageProvider storageProvider = new StorageProvider(objectMapper);
+  private final ObjectMapper objectMapper = new ObjectMapper();
+  public StorageProvider storageProvider = new StorageProvider(objectMapper);
 
-        @FunctionName("redirect")
-        public HttpResponseMessage run(
-                        @HttpTrigger(name = "req", methods = {
-                                        HttpMethod.GET }, authLevel = AuthorizationLevel.ANONYMOUS, route = "{id}") HttpRequestMessage<Optional<String>> request,
-                        @BindingName("id") String id,
-                        final ExecutionContext context) throws Exception {
+  @FunctionName("redirect")
+  public HttpResponseMessage run(
+    @HttpTrigger(
+      name = "req",
+      methods = {HttpMethod.GET},
+      authLevel = AuthorizationLevel.ANONYMOUS,
+      route = "{id}")
+    HttpRequestMessage<Optional<String>> request,
+    @BindingName("id") String id,
+    final ExecutionContext context
+  ) throws Exception {
 
-                try {
-                        if (id == null || id.isBlank())
-                                throw new Exception("URL not found");
+  try {
+    this.checkId(id);
+    
+    Optional<String> destinyUrl = this.getRedirectURL(id);
+    this.checkDestinyUrl(destinyUrl);
 
-                        Optional<String> destinyUrl = this.getRedirectURL(id);
+    return this.createRedirectResponse(request, HttpStatus.FOUND, destinyUrl.get());
+  } catch (Exception e) {
+    ExceptionDTO exception = new ExceptionDTO(e.getMessage(), HttpStatus.NOT_FOUND);
+    return this.createResponse(request, HttpStatus.NOT_FOUND, exception);
+  }
+}
 
-                        if (destinyUrl.isEmpty())
-                                throw new Exception("URL expired");
+  private Optional<String> getRedirectURL(String id) throws Exception {
+    JsonFileDTO foundFile = this.storageProvider.findBlobByName(id);
 
-                        return request.createResponseBuilder(HttpStatus.FOUND) // 301
-                                        .header("Location", destinyUrl.get())
-                                        .build();
-                } catch (Exception e) {
-                        ExceptionDTO exception = new ExceptionDTO(e.getMessage(), HttpStatus.NOT_FOUND);
-                        return request.createResponseBuilder(HttpStatus.NOT_FOUND) // 404
-                                        .body(exception)
-                                        .build();
-                }
-        }
+    return foundFile.getExpiresDate().isAfter(LocalDateTime.now())
+      ? Optional.ofNullable(foundFile.getSourceURL())
+      : Optional.empty();
+  }
 
-        private Optional<String> getRedirectURL(String id) throws Exception {
-                JsonFileDTO foundFile = this.storageProvider.findBlobByName(id);
+  private void checkId(String id) throws Exception {
+    if (id == null || id.isBlank())
+      throw new Exception("URL not found");
+  }
 
-                return foundFile.getExpiresDate().isAfter(LocalDateTime.now())
-                                ? Optional.ofNullable(foundFile.getSourceURL())
-                                : Optional.empty();
-        }
+  private void checkDestinyUrl(Optional<String> url) throws Exception {
+    if (url.isEmpty())
+      throw new Exception("URL expired");
+  }
+
+  private <T> HttpResponseMessage createRedirectResponse(HttpRequestMessage<T> request, HttpStatus status, String url) {
+    return request.createResponseBuilder(HttpStatus.FOUND)
+      .header("Location", url)
+      .build();
+  }
+  
+  private <T> HttpResponseMessage createResponse(HttpRequestMessage<T> request, HttpStatus status, Object body) {
+    return request.createResponseBuilder(HttpStatus.FOUND)
+      .body(body)
+      .build();
+  }
 }
